f533fa3aa64719eda9a79ce9e940bd958b7226b8
Armando Peña Tamayo <arman2p284@gmail.com>
2023-08-14 00:06:20 -0400

    Feat: Adding more styles
    Adding variables file to access material colors to css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -31,6 +34,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatGridListModule,
+    MatRippleModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
