511566ed47bd0a8c264888629da77db6da65dd20
Armando Peña Tamayo <arman2p284@gmail.com>
2023-08-19 18:56:17 -0400

    Feat: Add support for code snippets in interface
    Fix: Change question title to subject to manage better statistics

--- src/app/interfaces/question.ts
+++ src/app/interfaces/question.ts
@@ -2,8 +2,9 @@ import { Answer } from "./answer";
 import { level } from "./level";
 
 export interface Question {
-  title: string,
+  subject: string,
   body: string,
+  code_md_snippet?: string,
   level: level,
   answers: [Answer, Answer, Answer],
   response?: boolean,
