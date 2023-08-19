d20d086a11130de0e8049ab99f1b6d0b833b6f57
Armando Peña Tamayo <arman2p284@gmail.com>
2023-08-19 18:53:01 -0400

    Feat: Add level tags and progress bars to results
    This improves legibility

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { level } from 'src/app/interfaces/level';
+import { Question } from 'src/app/interfaces/question';
+import { ResultsService } from 'src/app/services/results.service';
+
+
+type color = 'primary' | 'accent' | 'warn';
 
 @Component({
   selector: 'app-results',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent {
+  isSurveyFinished!: boolean;
+  resultsByLevel!: Map<level, number>;
+  totalQuestionsByLevel!: Map<level, number>;
+  wrongQuestionsTitles!: string[];
 
+  constructor(private resultsService: ResultsService) { }
+
+  ngOnInit() {
+    const data = this.resultsService.getResults();
+    this.isSurveyFinished = data.isSurveyFinished;
+    this.resultsByLevel = data.resultsByLevel;
+    this.totalQuestionsByLevel = data.totalQuestionsByLevel;
+    this.wrongQuestionsTitles = data.wrongQuestions;
+  }
+
+  tagColor(level: level): color {
+    const colors = {
+      'junior': 'primary',
+      'semi-senior': 'accent',
+      'senior': 'warn',
+    };
+    return colors[level] as color;
+  }
+
+  getValue(value: number, total: number = 100) {
+    return value * 100 / total;
+  }
 }
