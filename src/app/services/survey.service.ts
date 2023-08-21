f3126696003f8cb549e786d059455bd590c9980b
Armando Peña Tamayo <arman2p284@gmail.com>
2023-08-21 15:34:05 -0400

    Feat: Adding helper methods to service

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -3,6 +3,7 @@ import { Survey } from '../interfaces/survey';
 import { Observable, of } from 'rxjs';
 import { mockSurvey } from './mock-questions';
 import { Question } from '../interfaces/question';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,22 @@ import { Question } from '../interfaces/question';
 export class SurveyService {
   constructor() { }
 
-  getSurvey(language: string): Observable<Survey> {
+  get fullSurvey(): Observable<Survey> {
+    return of(mockSurvey);
+  }
+
+  private getSurvey(language: string): Observable<Survey> {
     // TODO: http request to the server for the survey
     const mySurvey: Survey = JSON.parse(JSON.stringify(mockSurvey));
     const junior = mySurvey.questions.filter(data => data.level === 'junior');
     const semiSenior = mySurvey.questions.filter(data => data.level === 'semi-senior');
     const senior = mySurvey.questions.filter(data => data.level === 'senior');
-    const QUESTIONS_PER_LEVEL = 5;
 
     const getFiveRandomQuestions = (arr: Question[]): Question[] => {
       const response: Question[] = [];
       let count = 0;
       const usedIdx: number[] = [];
-      while (response.length < QUESTIONS_PER_LEVEL && count < arr.length) {
+      while (response.length < environment.questionsPerLevel && count < arr.length) {
         let idx: number;
         do {
           idx = Math.floor(Math.random() * arr.length);
@@ -35,6 +39,10 @@ export class SurveyService {
     }
     mySurvey.questions = [...getFiveRandomQuestions(junior), ...getFiveRandomQuestions(semiSenior), ...getFiveRandomQuestions(senior),];
     return of(mySurvey);
+  }
+
+  get javascriptSurvey(): Observable<Survey> {
+    return this.getSurvey('javascript');
   }
 
 }
